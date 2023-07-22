c59b3151e76abed232134d8729980232a252e5d7
Ankit Kumar Pandit <panditankit1995@gmail.com>
2023-07-22 20:17:32 +0530

    Progress style fix

--- packages/frontend/src/components/progress/Progress.tsx
+++ packages/frontend/src/components/progress/Progress.tsx
@@ -9,11 +9,16 @@ function Progress(props: ProgressProps) {
   const { value, full } = props;
   return (
     <div
-      className={cs('h-1 w-full bg-slate-950 transition-all', {
+      className={cs('h-1 w-full bg-slate-100 dark:bg-slate-950 transition-all', {
         'fixed w-screen h-screen flex items-center justify-center': full,
       })}
     >
-      <div className={cs({ 'w-[300px] border-4 border-slate-300': full, 'w-full': !full })}>
+      <div
+        className={cs({
+          'w-[420px] border-4 border-slate-300': full,
+          'w-full': !full,
+        })}
+      >
         <div className={cs('h-1 bg-green-600', { 'h-4': full })} style={{ width: `${value}%` }} />
       </div>
     </div>
