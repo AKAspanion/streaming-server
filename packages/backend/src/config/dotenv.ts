b3407c65bfe137af276df83dfa448a813f85bc84
Ankit Kumar Pandit <panditankit1995@gmail.com>
2023-07-29 13:46:15 +0530

    Build changes

--- packages/backend/src/config/dotenv.ts
+++ packages/backend/src/config/dotenv.ts
@@ -2,7 +2,7 @@ import path from 'path';
 import { IS_DEV } from './app';
 
 const devEnvPath = path.join(__dirname, '../../.env.development');
-const prodEnvPath = path.join(__dirname, '.env.production');
+const prodEnvPath = path.join(__dirname, '../../.env.production');
 
 export const dotenvConfig = {
   path: IS_DEV ? devEnvPath : prodEnvPath,
