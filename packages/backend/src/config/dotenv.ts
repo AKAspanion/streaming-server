94a80345f4cf8fb8ae91daa9170c48a5e6693bf0
Ankit Kumar Pandit <panditankit1995@gmail.com>
2023-08-26 23:14:48 +0530

    Logs removed

--- packages/backend/src/config/dotenv.ts
+++ packages/backend/src/config/dotenv.ts
@@ -4,8 +4,6 @@ import { IS_DEV } from './app';
 const devEnvPath = path.join(__dirname, '../../.env.development');
 const prodEnvPath = path.join(__dirname, '../../.env.production');
 
-console.log({ IS_DEV });
-
 export const dotenvConfig = {
   path: IS_DEV ? devEnvPath : prodEnvPath,
 };
