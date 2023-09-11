262cfc7798f40554742828adfe98c36e62599651
Ankit Kumar Pandit <panditankit1995@gmail.com>
2023-09-11 21:53:49 +0530

    GH pages deploy workflow updated

--- pages/src/routes/+page.svelte
+++ pages/src/routes/+page.svelte
@@ -1,10 +1,8 @@
-
-<h1 class="text-3xl font-bold">
-    Video Streaming Server
-</h1>
+<h1 class="text-3xl font-bold">Video Streaming Server</h1>
+<p>Landing page coming soon</p>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
